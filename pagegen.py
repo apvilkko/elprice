a6477bc0ebe1ca89d31b3ba3e9a5f659f7dd8ecb
Antti Pekka Vilkko <apvilkko@gmail.com>
2023-08-27 13:48:26 +0300

    feat: use icons

--- pagegen.py
+++ pagegen.py
@@ -127,6 +127,22 @@ def calc_average(data):
     return None
 
 
+def arrow_up():
+    return f"""
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-circle-fill" viewBox="0 0 16 16">
+  <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
+</svg>
+"""
+
+
+def arrow_down():
+    return f"""
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-circle-fill" viewBox="0 0 16 16">
+  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
+</svg>
+"""
+
+
 def calc_range(data, x, now: datetime):
     items = [y for day in data for y in day]
     items = [y for y in items if y[0] >= now.hour]  # only look at future
@@ -183,9 +199,9 @@ def render_cell(i: int, t: int, rows, stats):
     min = stats['min'][i]
     percent = None if cents is None else round(
         100 * ((cents-min) / (stats['max'][i] - min)))
-    pk_max_val = "🠉" if stats['pk'][i][hour] == stats['pk_max'][i] and \
+    pk_max_val = arrow_up() if stats['pk'][i][hour] == stats['pk_max'][i] and \
         stats['pk_max'][i] is not None else ""
-    pk_min_val = "🠋" if stats['pk'][i][hour] == stats['pk_min'][i] and \
+    pk_min_val = arrow_down() if stats['pk'][i][hour] == stats['pk_min'][i] and \
         stats['pk_min'][i] is not None else ""
     color = rgb2hex(color_for_price(cents))
     inv_percent = None if percent is None else 100 - percent
